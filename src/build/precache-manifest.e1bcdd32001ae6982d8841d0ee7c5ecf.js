5e234a00699b9e643d305ee2293f039537228c41
Dhyey Mistri <dhyeymistri27@gmail.com>
2022-07-16 18:21:55 +0530

    Full site done, first commit

---
+++ src/build/precache-manifest.e1bcdd32001ae6982d8841d0ee7c5ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a51153d74ec37746b9",
+    "url": "/static/css/main.0f13461b.chunk.css"
+  },
+  {
+    "revision": "a5a51153d74ec37746b9",
+    "url": "/static/js/main.8cc5fd9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48adc3372eeab7ab1a10",
+    "url": "/static/js/2.b89b5ff5.chunk.js"
+  },
+  {
+    "revision": "aca6b505983ebe66eada22043a77fdaa",
+    "url": "/index.html"
+  }
+];
